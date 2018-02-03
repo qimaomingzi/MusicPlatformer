a8c3a908f281fe6c354623e72a194efa7507133c
Unknown <okh216@nyu.edu>
2018-02-03 17:23:15 -0500

    Q Key not working, slight change to lockingplatform code
    Used "Keycode" in order to specify "Q" button.

--- Assets/Scripts/LockPlatforms.cs
+++ Assets/Scripts/LockPlatforms.cs
@@ -17,7 +17,7 @@ public class LockPlatforms : MonoBehaviour {
     private void OnTriggerStay2D(Collider2D collision)
     {
         if(collision.gameObject.tag == "Platform"){
-            if(Input.GetKeyDown("q")){
+            if(Input.GetKeyDown(KeyCode.Q)){
                 collision.gameObject.GetComponent<MovingBlock>().enabled = false;
             }
         }
