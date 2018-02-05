9be06cddb5a99cca3280870b8a6ac0505411da0f
Unknown <okh216@nyu.edu>
2018-02-05 16:14:26 -0500

    Working on collider...
    Not sure why Linecheck isn't colliding with linecheckend...

---
+++ Assets/Scripts/LineBeatMover.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineBeatMover : MonoBehaviour {
+    public float speed = 1.0f;
+    public float currentXposition;
+    public float lineLimiter;
+    public float startingSpot;
+    Collider2D collision;
+    Rigidbody2D rb2d;
+	// Use this for initialization
+	void Start ()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        collision = GetComponent<Collider2D>();
+	}
+
+    private void FixedUpdate()
+    {
+        rb2d.velocity = new Vector2(speed * Time.deltaTime, 0);
+    }
+
+    // Update is called once per frame
+    void Update () { 
+	}
+}
